9c948d8e16c5ffa6fe7493034d5e375c8dc69ec9
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-10-25 20:08:00 +0300

    added part 1 Lesson-27

--- Lesson_27/ViewController.swift
+++ Lesson_27/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet private weak var mapView: MKMapView!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            mapView.register(ImageAnnotation.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+            mapView.delegate = self
+        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            mapView.showsUserLocation = true
+            
+            let center = CLLocationCoordinate2D(latitude: 37.785834, longitude: -122.406417)
+            let camera = MKMapCamera(lookingAtCenter: center, fromDistance: CLLocationDistance(15), pitch: 10, heading: CLLocationDirection(1))
+            mapView.setCamera(camera, animated: false)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = center
+            annotation.title = "T-Mobile"
+            mapView.addAnnotation(annotation)
+        
+            
+        }
+    
+//    изменение на кастомный pin
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let annotationView = ImageAnnotation(
+                annotation: annotation,
+                reuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier
+            )
+            annotationView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            return annotationView
+        }
     }
 
-
+class ImageAnnotation: MKAnnotationView {
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+        image = UIImage(named: "ozero_5")
+        
+        let label = UILabel()
+        label.text = "123"
+        detailCalloutAccessoryView = label
+        canShowCallout = true
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
-
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Lesson_27' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Lesson_27
+  pod 'GoogleMaps', '5.2.0'
+end
